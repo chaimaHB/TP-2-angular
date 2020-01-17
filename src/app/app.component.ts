3e47c692bdc4c3fce97eeb39fc79fa750141bc27
chaima habibi <chaimahabibi17@gmail.com>
2020-01-18 00:39:56 +0100

    first-project-master

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  isAuth = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 4000)
+  }
+
+  onAllume() {
+    console.log('click');
+  }
+}
